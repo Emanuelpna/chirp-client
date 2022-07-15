7b7598b80948151de361f676cb5f11f61f34cce8
Emanuel Andrade <emanuelpna@yahoo.com.br>
2022-07-15 15:30:00 -0300

    refactor(layout): adjusted total width of layout

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,10 +1,18 @@
 import styled from 'styled-components'
 
 export const Main = styled.main`
-  width: 100%;
-  padding: 0 20%;
-  padding-bottom: 10%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 0 8%;
+  margin: 0 auto;
+  max-width: 1000px;
   min-height: 100vh;
+  padding-bottom: 10%;
+
+  & > div {
+    width: 80%;
+  }
 `
 
 export const Header = styled.header`
